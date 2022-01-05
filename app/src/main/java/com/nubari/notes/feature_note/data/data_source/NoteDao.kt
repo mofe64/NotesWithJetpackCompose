b4965cd206a579338b6577f5ef235f0eb4eea466
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2022-01-05 14:08:35 +0100

    note dao

--- app/src/main/java/com/nubari/notes/feature_note/data/data_source/NoteDao.kt
+++ app/src/main/java/com/nubari/notes/feature_note/data/data_source/NoteDao.kt
@@ -1,4 +1,25 @@
 package com.nubari.notes.feature_note.data.data_source
 
+import androidx.room.*
+import com.nubari.notes.feature_note.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface NoteDao {
+    @Query("SELECT * FROM note")
+    fun getNotes(): Flow<List<Note>>
+
+    /*
+    * This is a suspend function because we are just directly
+    * returning the node, and we are not wrap it around a
+    * flow object
+    * */
+    @Query("SELECT * FROM note WHERE id = :id")
+    suspend fun getNoteById(id: Int): Note?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
 }
